978fce5d3be3a901dcc3f4c0d4378ad9e5eccc7c
thomasms <stainer.tom@gmail.com>
2018-10-13 15:38:42 +0100

    Removed hover from CSS to using events instead.

--- src/Element.js
+++ src/Element.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import './Element.css';
 
-function Element(props) {
-  return (
-    <div className="Element" style={props.style}>
-      <div className="Element-atomic">
-        {props.atomic}
+class Element extends React.Component {
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      symbol: props.symbol,
+      atomic: props.atomic,
+      gridPosition: props.gridPosition,
+      style: {
+          gridColumn: props.gridPosition,
+          gridColumnEnd: props.gridPosition,
+          borderColor: "white"
+      },
+      isSelected: false
+    }
+
+    this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this);
+    this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
+  }
+
+  handleOnMouseEnter(){
+    this.setState((prevState, props) => {
+      return {
+              isSelected: true,
+              style: {
+                  gridColumn: prevState.gridPosition,
+                  gridColumnEnd: prevState.gridPosition,
+                  borderColor: "red"
+              }
+             };
+    });
+  }
+
+  handleOnMouseLeave(){
+    this.setState((prevState, props) => {
+      return {
+              isSelected: true,
+              style: {
+                  gridColumn: prevState.gridPosition,
+                  gridColumnEnd: prevState.gridPosition,
+                  borderColor: "white"
+              }
+             };
+    });
+  }
+
+  render(){
+    return (
+      <div className="Element"
+            style={this.state.style}
+            onMouseEnter={this.handleOnMouseEnter}
+            onMouseLeave={this.handleOnMouseLeave}>
+        <div className="Element-atomic">
+          {this.state.atomic}
+        </div>
+        <div className="Element-symbol">
+          {this.state.symbol}
+        </div>
       </div>
-      <div className="Element-symbol">
-        {props.symbol}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Element;
--- src/Table.js
+++ src/Table.js
@@ -12,7 +12,7 @@ function TableRowLeftElements(data){
           key={i}
           symbol={data[i].symbol}
           atomic={data[i].atomic}
-          style={{gridColumnStart: i+1, gridColumnEnd: i+1}}
+          gridPosition={i+1}
         />
       );
   }
@@ -32,7 +32,7 @@ function TableRowRightElements(data){
           key={MAX_LENGTH-i}
           symbol={data[i].symbol}
           atomic={data[i].atomic}
-          style={{gridColumnStart: MAX_LENGTH-data.length+i+1, gridColumnEnd: MAX_LENGTH-data.length+i+1}}
+          gridPosition={MAX_LENGTH-data.length+i+1}
         />
       );
   }
@@ -44,69 +44,91 @@ function TableRowRightElements(data){
   );
 }
 
-function Table(props) {
-  return (
-    <div className="Table">
-      {TableRowLeftElements([{symbol: "H", atomic: 1}])}
-      {TableRowRightElements([{symbol: "He", atomic: 2}])}
+class Table extends React.Component {
 
-      {TableRowLeftElements([{symbol: "Li", atomic: 3},
-                             {symbol: "Be", atomic: 4}])}
-      {TableRowRightElements([{symbol: "B", atomic: 5},
-                              {symbol: "C", atomic: 6},
-                              {symbol: "N", atomic: 7},
-                              {symbol: "O", atomic: 8},
-                              {symbol: "F", atomic: 9},
-                              {symbol: "Ne", atomic: 10}])}
+  constructor( props ) {
+    super( props );
 
-      {TableRowLeftElements([{symbol: "Na", atomic: 11},
-                             {symbol: "Mg", atomic: 12}])}
-      {TableRowRightElements([{symbol: "Al", atomic: 13},
-                              {symbol: "Si", atomic: 14},
-                              {symbol: "P", atomic: 15},
-                              {symbol: "S", atomic: 16},
-                              {symbol: "Cl", atomic: 17},
-                              {symbol: "Ar", atomic: 18}])}
+    this.state = {
+      selectedElement: ""
+    }
 
-      {TableRowLeftElements([{symbol: "K", atomic: 19},
-                             {symbol: "Ca", atomic: 20},
-                             {symbol: "Sc", atomic: 21},
-                             {symbol: "Ti", atomic: 22},
-                             {symbol: "V", atomic: 23},
-                             {symbol: "Cr", atomic: 24},
-                             {symbol: "Mn", atomic: 25},
-                             {symbol: "Fe", atomic: 26},
-                             {symbol: "Co", atomic: 27},
-                             {symbol: "Ni", atomic: 28},
-                             {symbol: "Cu", atomic: 29},
-                             {symbol: "Zn", atomic: 30},
-                             {symbol: "Ga", atomic: 31},
-                             {symbol: "Ge", atomic: 32},
-                             {symbol: "As", atomic: 33},
-                             {symbol: "Se", atomic: 34},
-                             {symbol: "Br", atomic: 35},
-                             {symbol: "Kr", atomic: 36}])}
+    this.handleSelectedElement = this.handleSelectedElement.bind(this);
+  }
 
-     {TableRowLeftElements([{symbol: "Rb", atomic: 37},
-                            {symbol: "Sr", atomic: 38},
-                            {symbol: "Y", atomic: 39},
-                            {symbol: "Zr", atomic: 40},
-                            {symbol: "Nb", atomic: 41},
-                            {symbol: "Mo", atomic: 42},
-                            {symbol: "Tc", atomic: 43},
-                            {symbol: "Ru", atomic: 44},
-                            {symbol: "Rh", atomic: 45},
-                            {symbol: "Pd", atomic: 46},
-                            {symbol: "Ag", atomic: 47},
-                            {symbol: "Cd", atomic: 48},
-                            {symbol: "In", atomic: 49},
-                            {symbol: "Sn", atomic: 50},
-                            {symbol: "Sb", atomic: 51},
-                            {symbol: "Te", atomic: 52},
-                            {symbol: "I", atomic: 53},
-                            {symbol: "Xe", atomic: 54}])}
-    </div>
-  );
+  handleSelectedElement(){
+
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="Table">
+          {TableRowLeftElements([{symbol: "H", atomic: 1}])}
+          {TableRowRightElements([{symbol: "He", atomic: 2}])}
+
+          {TableRowLeftElements([{symbol: "Li", atomic: 3},
+                                 {symbol: "Be", atomic: 4}])}
+          {TableRowRightElements([{symbol: "B", atomic: 5},
+                                  {symbol: "C", atomic: 6},
+                                  {symbol: "N", atomic: 7},
+                                  {symbol: "O", atomic: 8},
+                                  {symbol: "F", atomic: 9},
+                                  {symbol: "Ne", atomic: 10}])}
+
+          {TableRowLeftElements([{symbol: "Na", atomic: 11},
+                                 {symbol: "Mg", atomic: 12}])}
+          {TableRowRightElements([{symbol: "Al", atomic: 13},
+                                  {symbol: "Si", atomic: 14},
+                                  {symbol: "P", atomic: 15},
+                                  {symbol: "S", atomic: 16},
+                                  {symbol: "Cl", atomic: 17},
+                                  {symbol: "Ar", atomic: 18}])}
+
+          {TableRowLeftElements([{symbol: "K", atomic: 19},
+                                 {symbol: "Ca", atomic: 20},
+                                 {symbol: "Sc", atomic: 21},
+                                 {symbol: "Ti", atomic: 22},
+                                 {symbol: "V", atomic: 23},
+                                 {symbol: "Cr", atomic: 24},
+                                 {symbol: "Mn", atomic: 25},
+                                 {symbol: "Fe", atomic: 26},
+                                 {symbol: "Co", atomic: 27},
+                                 {symbol: "Ni", atomic: 28},
+                                 {symbol: "Cu", atomic: 29},
+                                 {symbol: "Zn", atomic: 30},
+                                 {symbol: "Ga", atomic: 31},
+                                 {symbol: "Ge", atomic: 32},
+                                 {symbol: "As", atomic: 33},
+                                 {symbol: "Se", atomic: 34},
+                                 {symbol: "Br", atomic: 35},
+                                 {symbol: "Kr", atomic: 36}])}
+
+         {TableRowLeftElements([{symbol: "Rb", atomic: 37},
+                                {symbol: "Sr", atomic: 38},
+                                {symbol: "Y", atomic: 39},
+                                {symbol: "Zr", atomic: 40},
+                                {symbol: "Nb", atomic: 41},
+                                {symbol: "Mo", atomic: 42},
+                                {symbol: "Tc", atomic: 43},
+                                {symbol: "Ru", atomic: 44},
+                                {symbol: "Rh", atomic: 45},
+                                {symbol: "Pd", atomic: 46},
+                                {symbol: "Ag", atomic: 47},
+                                {symbol: "Cd", atomic: 48},
+                                {symbol: "In", atomic: 49},
+                                {symbol: "Sn", atomic: 50},
+                                {symbol: "Sb", atomic: 51},
+                                {symbol: "Te", atomic: 52},
+                                {symbol: "I", atomic: 53},
+                                {symbol: "Xe", atomic: 54}])}
+        </div>
+        <h3>
+          {"H"}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default Table;
