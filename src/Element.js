6fa9b4b1b936617f42d049f3c3be8c371e7fa67c
thomasms <stainer.tom@gmail.com>
2018-10-17 22:09:12 +0100

    Removal of state from Element. Refactoring and fixes and improvements.

--- src/Element.js
+++ src/Element.js
@@ -1,96 +1,28 @@
 import React from 'react';
 import './Element.css';
 
-class Element extends React.Component {
+function GridColourElement(props){
 
-  constructor( props ) {
-    super( props );
+  const style = {
+      gridColumn: props.gridPosition,
+      gridColumnEnd: props.gridPosition,
+      borderColor: props.colour
+  };
 
-    this.state = {
-      symbol: props.symbol,
-      atomic: props.atomic,
-      gridPosition: props.gridPosition,
-      isSelected: false,
-      isHovered: false,
-      parentClickHandler: props.clickHandler,
-      parentHoverHandler: props.hoverHandler
-    }
-
-    this.handleOnClick      = this.handleOnClick.bind(this);
-    this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this);
-    this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
-  }
-
-  handleOnClick(){
-    this.setState((prevState, props) => {
-
-      // call parent hover handler to pass
-      // element details upwards
-      if(prevState.parentClickHandler){
-        prevState.parentClickHandler(prevState);
-      }
-
-      return { isSelected: !prevState.isSelected };
-    });
-  }
-
-  handleOnMouseEnter(){
-
-    this.setState((prevState, props) => {
-
-      // call parent hover handler to pass
-      // element details upwards
-      if(prevState.parentHoverHandler){
-        prevState.parentHoverHandler(prevState);
-      }
-
-      return { isHovered: true };
-    });
-  }
-
-  handleOnMouseLeave(){
-    this.setState((prevState, props) => {
-
-      // call parent hover handler to pass
-      // null element upwards
-      if(prevState.parentHoverHandler){
-        prevState.parentHoverHandler(null);
-      }
-
-      return { isHovered: false };
-    });
-  }
-
-  render(){
-    var colour = "white";
-    if(this.state.isHovered){
-      colour = "red";
-    }
-    if(this.state.isSelected){
-      colour = "blue";
-    }
-
-    const style = {
-        gridColumn: this.state.gridPosition,
-        gridColumnEnd: this.state.gridPosition,
-        borderColor: colour
-    };
-
-    return (
-      <div className="Element"
-            style={style}
-            onMouseEnter={this.handleOnMouseEnter}
-            onMouseLeave={this.handleOnMouseLeave}
-            onClick={this.handleOnClick}>
-        <div className="Element-atomic">
-          {this.state.atomic}
-        </div>
-        <div className="Element-symbol">
-          {this.state.symbol}
-        </div>
+  return(
+    <div className="Element"
+          style={style}
+          onMouseEnter={(element) => props.handleOnMouseEnter(props.element)}
+          onMouseLeave={(element) => props.handleOnMouseLeave(props.element)}
+          onClick={(element) => props.handleOnClick(props.element)}>
+      <div className="Element-atomic">
+        {props.element.atomic}
       </div>
-    );
-  }
+      <div className="Element-symbol">
+        {props.element.symbol}
+      </div>
+    </div>
+  );
 }
 
-export default Element;
+export { GridColourElement };
--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Element from './Element.js';
+import { GridColourElement } from './Element.js';
 import './Table.css';
 
 const MAX_LENGTH = 18;
@@ -10,153 +10,156 @@ class Table extends React.Component {
     super( props );
 
     this.state = {
-      hoveredElement: "",
+      hoveredElement: null,
       selectedElements: []
     }
-
-    this.handleHoveredElement = this.handleHoveredElement.bind(this);
-    this.handleSelectedElement = this.handleSelectedElement.bind(this);
   }
 
-   rowLeftElements(data, handler){
-    var elements = [];
-    for(var i = 0; i < data.length; i++) {
-        elements.push(
-          <Element
-            key={i}
-            symbol={data[i].symbol}
-            atomic={data[i].atomic}
-            gridPosition={i+1}
-            hoverHandler={this.handleHoveredElement}
-            clickHandler={this.handleSelectedElement}
-          />
-        );
-    }
+  createElements(data, offset){
+   var elements = [];
+   for(var i = 0; i < data.length; i++) {
+       var colour = "white";
+       if(this.state.hoveredElement &&
+          this.state.hoveredElement.symbol === data[i].symbol){
+         colour = "blue";
+       }
+       if(this.state.selectedElements.indexOf(data[i].symbol) > -1){
+         colour = "red";
+       }
 
+       elements.push(
+         <GridColourElement
+           key={i}
+           colour={colour}
+           element={data[i]}
+           gridPosition={offset+i+1}
+           handleOnMouseEnter={(element) => {this.setState({hoveredElement: element})}}
+           handleOnMouseLeave={(element) => {this.setState({hoveredElement: null})}}
+           handleOnClick={(element) => {
+              this.setState((prevState, props) => {
+                var elements = prevState.selectedElements;
+                if(element){
+                  if(elements.indexOf(element.symbol) === -1){
+                    elements.push(element.symbol);
+                  }
+                  else{
+                    elements = elements.filter(function(value, index, arr){
+                        return value !== element.symbol;
+                    });
+                  }
+                }
+
+                return { selectedElements: elements };
+              });
+            }
+           }
+         />
+       );
+   }
+   return elements;
+  }
+
+   rowLeftElements(data){
     return (
       <>
-      {elements}
+      {this.createElements(data, 0)}
       </>
     );
   }
 
-   rowRightElements(data, handler){
-    var elements = [];
-    for(var i = 0; i < data.length; i++) {
-        elements.push(
-          <Element
-            key={MAX_LENGTH-i}
-            symbol={data[i].symbol}
-            atomic={data[i].atomic}
-            gridPosition={MAX_LENGTH-data.length+i+1}
-            hoverHandler={this.handleHoveredElement}
-            clickHandler={this.handleSelectedElement}
-          />
-        );
-    }
-
+   rowRightElements(data){
     return (
       <>
-      {elements}
+      {this.createElements(data, MAX_LENGTH-data.length)}
       </>
     );
   }
 
-  handleHoveredElement(element){
-    this.setState({hoveredElement: element});
-  }
+  allElements(){
+    return (
+      <>
+      {this.rowLeftElements([{symbol: "H", atomic: 1, name: "Hydrogen"}])}
+      {this.rowRightElements([{symbol: "He", atomic: 2, name: "Helium"}])}
 
-  handleSelectedElement(element){
-    this.setState((prevState, props) => {
+      {this.rowLeftElements([{symbol: "Li", atomic: 3, name: "Lithium"},
+                             {symbol: "Be", atomic: 4, name: "Beryllium"}])}
+      {this.rowRightElements([{symbol: "B", atomic: 5, name: "Boron"},
+                              {symbol: "C", atomic: 6, name: "Carbon"},
+                              {symbol: "N", atomic: 7, name: "Nitrogen"},
+                              {symbol: "O", atomic: 8, name: "Oxygen"},
+                              {symbol: "F", atomic: 9, name: "Fluorine"},
+                              {symbol: "Ne", atomic: 10, name: "Neon"}])}
 
-      var elements = prevState.selectedElements;
-      if(element){
-        if(!element.isSelected){
-          elements.push(element.symbol);
-        }
-        else{
-          elements = elements.filter(function(value, index, arr){
-              return value !== element.symbol;
-          });
-        }
-      }
+      {this.rowLeftElements([{symbol: "Na", atomic: 11, name: "Sodium"},
+                             {symbol: "Mg", atomic: 12, name: "Magnesium"}])}
+      {this.rowRightElements([{symbol: "Al", atomic: 13},
+                              {symbol: "Si", atomic: 14},
+                              {symbol: "P", atomic: 15},
+                              {symbol: "S", atomic: 16},
+                              {symbol: "Cl", atomic: 17},
+                              {symbol: "Ar", atomic: 18}])}
 
-      return { selectedElements: elements };
-    });
+      {this.rowLeftElements([{symbol: "K", atomic: 19},
+                             {symbol: "Ca", atomic: 20},
+                             {symbol: "Sc", atomic: 21},
+                             {symbol: "Ti", atomic: 22},
+                             {symbol: "V", atomic: 23},
+                             {symbol: "Cr", atomic: 24},
+                             {symbol: "Mn", atomic: 25},
+                             {symbol: "Fe", atomic: 26},
+                             {symbol: "Co", atomic: 27},
+                             {symbol: "Ni", atomic: 28},
+                             {symbol: "Cu", atomic: 29},
+                             {symbol: "Zn", atomic: 30},
+                             {symbol: "Ga", atomic: 31},
+                             {symbol: "Ge", atomic: 32},
+                             {symbol: "As", atomic: 33},
+                             {symbol: "Se", atomic: 34},
+                             {symbol: "Br", atomic: 35},
+                             {symbol: "Kr", atomic: 36}])}
+
+     {this.rowLeftElements([{symbol: "Rb", atomic: 37},
+                            {symbol: "Sr", atomic: 38},
+                            {symbol: "Y", atomic: 39},
+                            {symbol: "Zr", atomic: 40},
+                            {symbol: "Nb", atomic: 41},
+                            {symbol: "Mo", atomic: 42},
+                            {symbol: "Tc", atomic: 43},
+                            {symbol: "Ru", atomic: 44},
+                            {symbol: "Rh", atomic: 45},
+                            {symbol: "Pd", atomic: 46},
+                            {symbol: "Ag", atomic: 47},
+                            {symbol: "Cd", atomic: 48},
+                            {symbol: "In", atomic: 49},
+                            {symbol: "Sn", atomic: 50},
+                            {symbol: "Sb", atomic: 51},
+                            {symbol: "Te", atomic: 52},
+                            {symbol: "I", atomic: 53},
+                            {symbol: "Xe", atomic: 54}])}
+      </>
+    );
   }
 
   render(){
     var details = "";
     if(this.state.hoveredElement){
-      details = this.state.hoveredElement.symbol;
+      details = this.state.hoveredElement.name;
     }
 
     var selectedElements = "";
     for(var i = 0; i < this.state.selectedElements.length; i++) {
-      selectedElements += this.state.selectedElements[i] + ", ";
+      selectedElements += this.state.selectedElements[i];
+      if(i < this.state.selectedElements.length-1){
+        selectedElements += ", ";
+      }
     }
 
+    const allElements = this.allElements();
     return (
       <div>
+        <button onClick={() => {this.setState({selectedElements: []})}}>Reset</button>
         <div className="Table">
-          {this.rowLeftElements([{symbol: "H", atomic: 1}])}
-          {this.rowRightElements([{symbol: "He", atomic: 2}])}
-
-          {this.rowLeftElements([{symbol: "Li", atomic: 3},
-                                 {symbol: "Be", atomic: 4}])}
-          {this.rowRightElements([{symbol: "B", atomic: 5},
-                                  {symbol: "C", atomic: 6},
-                                  {symbol: "N", atomic: 7},
-                                  {symbol: "O", atomic: 8},
-                                  {symbol: "F", atomic: 9},
-                                  {symbol: "Ne", atomic: 10}])}
-
-          {this.rowLeftElements([{symbol: "Na", atomic: 11},
-                                 {symbol: "Mg", atomic: 12}])}
-          {this.rowRightElements([{symbol: "Al", atomic: 13},
-                                  {symbol: "Si", atomic: 14},
-                                  {symbol: "P", atomic: 15},
-                                  {symbol: "S", atomic: 16},
-                                  {symbol: "Cl", atomic: 17},
-                                  {symbol: "Ar", atomic: 18}])}
-
-          {this.rowLeftElements([{symbol: "K", atomic: 19},
-                                 {symbol: "Ca", atomic: 20},
-                                 {symbol: "Sc", atomic: 21},
-                                 {symbol: "Ti", atomic: 22},
-                                 {symbol: "V", atomic: 23},
-                                 {symbol: "Cr", atomic: 24},
-                                 {symbol: "Mn", atomic: 25},
-                                 {symbol: "Fe", atomic: 26},
-                                 {symbol: "Co", atomic: 27},
-                                 {symbol: "Ni", atomic: 28},
-                                 {symbol: "Cu", atomic: 29},
-                                 {symbol: "Zn", atomic: 30},
-                                 {symbol: "Ga", atomic: 31},
-                                 {symbol: "Ge", atomic: 32},
-                                 {symbol: "As", atomic: 33},
-                                 {symbol: "Se", atomic: 34},
-                                 {symbol: "Br", atomic: 35},
-                                 {symbol: "Kr", atomic: 36}])}
-
-         {this.rowLeftElements([{symbol: "Rb", atomic: 37},
-                                {symbol: "Sr", atomic: 38},
-                                {symbol: "Y", atomic: 39},
-                                {symbol: "Zr", atomic: 40},
-                                {symbol: "Nb", atomic: 41},
-                                {symbol: "Mo", atomic: 42},
-                                {symbol: "Tc", atomic: 43},
-                                {symbol: "Ru", atomic: 44},
-                                {symbol: "Rh", atomic: 45},
-                                {symbol: "Pd", atomic: 46},
-                                {symbol: "Ag", atomic: 47},
-                                {symbol: "Cd", atomic: 48},
-                                {symbol: "In", atomic: 49},
-                                {symbol: "Sn", atomic: 50},
-                                {symbol: "Sb", atomic: 51},
-                                {symbol: "Te", atomic: 52},
-                                {symbol: "I", atomic: 53},
-                                {symbol: "Xe", atomic: 54}])}
+          {allElements}
         </div>
         <h3>
           {details}
