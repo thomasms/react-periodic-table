aa4cd97948b0f285667a097ec980dda2d20182d4
thomasms <stainer.tom@gmail.com>
2018-10-24 21:58:13 +0100

    Added some simple rendering unit tests.

--- src/Element.test.js
+++ src/Element.test.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GridColourElement } from './Element.js';
 
-it('renders without crashing', () => {
+it('renders basic without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <GridColourElement
+    element={{atomic: 1, symbol: "H"}} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders with colour without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <GridColourElement
+      colour={"blue"}
+      element={{atomic: 3, symbol: "Li"}}/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders full props without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <GridColourElement
